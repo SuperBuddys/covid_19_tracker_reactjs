5b4baa129c84feb249c08dd021bd542eb3742d7e
RahulMohite <melayer.rahul27@gmail.com>
2020-04-07 04:40:18 +0530

    added chart for active cases and deaths ,added dropdown to select country

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,11 @@ import axios from 'axios'; //for api request
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+    if (country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
     try {
         //object de-structuring
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
@@ -14,8 +18,24 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
     try {
         const { data } = await axios.get(`${url}/daily`)
-        console.log("data ", data);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+        return modifiedData
     } catch (error) {
+        console.log("error ", error);
+
+    }
+}
+
+export const fetchCountries = async () => {
+    try {
+        const { data: { countries } } = await axios.get(`${url}/countries`)
+        return countries.map((country) => country.name)
+    } catch (error) {
+        console.log("error ", error);
 
     }
 }
