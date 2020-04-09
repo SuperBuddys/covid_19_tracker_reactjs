0cea7e89aed15aef3e49a095331894e69814a3b6
RahulMohite <melayer.rahul27@gmail.com>
2020-04-09 17:03:23 +0530

    Updates

---
+++ precache-manifest.2a8c2ba1e4cfa7ba03229ab613c33819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9fefe4562020a5dbff15231df08fdc",
+    "url": "/covid_19_tracker_reactjs/index.html"
+  },
+  {
+    "revision": "81550b95c1a245b60e75",
+    "url": "/covid_19_tracker_reactjs/static/css/main.79379ded.chunk.css"
+  },
+  {
+    "revision": "7bed4b10f362834a8908",
+    "url": "/covid_19_tracker_reactjs/static/js/2.7fedbfe2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_19_tracker_reactjs/static/js/2.7fedbfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81550b95c1a245b60e75",
+    "url": "/covid_19_tracker_reactjs/static/js/main.c78acbe9.chunk.js"
+  },
+  {
+    "revision": "1986ab0ca7f88866b331",
+    "url": "/covid_19_tracker_reactjs/static/js/runtime-main.131c7758.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_19_tracker_reactjs/static/media/image.d7265326.png"
+  }
+]);
